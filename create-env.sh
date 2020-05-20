b781b1beef6ec7f4fdbca6f878a1a9147a1aba3d
Aurelien <aurelien.benoist@yahoo.fr>
2020-05-20 20:52:57 +0000

    add docker container server

--- create-env.sh
+++ create-env.sh
@@ -7,6 +7,8 @@
 sudo echo -e "Creating your ec2 instances..."
 
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --user-data file://install-env.sh
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --user-data file://install-docker-env.sh
+
 Instances[0]=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=pending --query 'Reservations[*].Instances[0].{Instance:InstanceId}')
 
 echo -e "\nWaiting for the instances to be ready...\n"
